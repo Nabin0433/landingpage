0583b54dfac66d403c022552ca018705678f4b0d
nabin <nabin043@gmail.com>
2023-02-10 23:27:08 +0545

    fix-ui-add-img-modal

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -1,18 +1,50 @@
 import dynamic from 'next/dynamic';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 const Carousel = dynamic(
     () => import('flat-carousel'),
     { ssr: false }
 )
 
-const ImageSlider = ({ images }) => {
-    return (
-        <Carousel transitionDuration={300}>
-            {images.map(item => (
-                <img key={item.id} className='w-full h-full object-contain pointer-events-none' src={item.img} alt={item.name} />
-            ))}
-        </Carousel>
 
+const ImageSlider = ({ images }) => {
+    const [img, setImg] = useState(null)
+
+    useEffect(() => {
+        if (img) {
+            lockScroll()
+        } else {
+            removeLockScroll()
+        }
+    }, [img])
+
+    const lockScroll = () => {
+        document.body.classList.add('lock-scroll');
+    }
+
+    const removeLockScroll = () => {
+        document.body.classList.remove('lock-scroll');
+    }
+
+    return (
+        <>
+            <Carousel transitionDuration={300}>
+                {images.map(item => (
+                    <div onClick={() => setImg(item.img)}>
+                        <img key={item.id} className='w-full h-full object-contain pointer-events-none' src={item.img} alt={item.name} />
+                    </div>
+                ))}
+            </Carousel>
+            {img && (
+                <>
+                    <div className='fixed w-screen h-screen inset-0 z-20 bg-primary/95 flex justify-center items-center' onClick={() => setImg(null)}>
+                        <span className='text-[30px] text-white absolute top-3 lg:top-6 right-6 lg:right-8 font-[Montserrat] font-semibold cursor-pointer'>x</span>
+                        <img onClick={(e) => e.stopPropagation()} className='w-full max-h-[90vh] object-contain pointer-events-none' src={img} alt="Modal" />
+                    </div>
+                </>
+
+            )}
+
+        </>
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -31,8 +31,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Blue Peak Innovation</title>
-        <meta name="description" content="technology , build , create , website , it , solutions" />
+        <title>{data.page_title}</title>
+        <meta name="description" content={data.seo_description} />
         <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
@@ -52,7 +52,7 @@ export default function Home() {
           </div>
           <div className='lg:hidden pr-2 pt-2'>
             <Menu right width={'280px'} onOpen={() => setNavOpen(true)} onClose={() => setNavOpen(false)} isOpen={navOpen}>
-              <h2 className='text-[24px] absolute top-5 left-14'>Menu</h2>
+              <h6 className='text-[24px] absolute top-5 left-14'>Menu</h6>
               <div className='px-2 py-8 flex flex-col'>
                 <Link href={`#${data.section_1.id}`} onClick={clickNav} className='font-[Montserrat] block text-[16px] cursor-pointer hover:text-gray-300 py-3'>Home</Link>
                 {data.navs.map(item => (
@@ -67,8 +67,8 @@ export default function Home() {
             <img className='absolute md:ml-1 w-full h-[96%] -z-0 object-cover opacity-70' src={data.section_1.background_img} alt="background img" />
             <div className='flex justify-center items-center z-20 absolute inset-0'>
               <div className='text-white w-[330px] md:w-[800px] 2xl:w-[900px]'>
-                <h2 className='text-[28px] md:text-[40px] 2xl:text-[45px]'>{data.section_1.title}</h2>
-                <h3 className='text-[14px] md:text-[20px] 2xl:text-[25px] font-[Montserrat] mt-4'>{data.section_1.sub_title}</h3>
+                <h1 className='text-[28px] md:text-[40px] 2xl:text-[45px]'>{data.section_1.title}</h1>
+                <h2 className='text-[14px] md:text-[20px] 2xl:text-[25px] font-[Montserrat] mt-4'>{data.section_1.sub_title}</h2>
                 <div className='flex justify-center mt-12 md:mt-16'>
                   <a link={data.section_6.id} className='border font-[Montserrat] text-[14px] 2xl:text-[18px] border-white h-12 2xl:h-14 flex items-center justify-center px-4 2xl:px-6 hover:border-gray-300'>{data.section_1.learn_more}</a>
                 </div>
@@ -87,7 +87,6 @@ export default function Home() {
               <hr />
               <div className='pl-4 lg:w-[490px] 2xl:w-[600px]'>
                 <h3 className='text-[14px] md:text-[16px] lg:text-[14px] 2xl:text-[18px] font-light mt-4 font-[Montserrat] leading-normal'>{data.section_2.sub_title}</h3>
-                {/* <p className='text-[28px] text-center my-4'>{data.section_1.learn_more}</p> */}
                 <div className='flex gap-2 md:gap-8 items-start justify-center mt-12'>
                   {data.section_2.icons.map((item, index) => (
                     <div key={item.id} className='flex flex-col items-center justify-center'>
@@ -130,9 +129,6 @@ export default function Home() {
             </div>
             <div className='w-full lg:w-1/2'>
               <ImageSlider images={data.section_4.background_img} />
-              {/* {data.section_4.background_img.map(item => (
-                <img key={item.id} className='w-full h-[400px] md:h-[500px] lg:h-[600px] object-contain' src={item.img} alt={item.name} />
-              ))} */}
             </div>
           </div>
         </section>
